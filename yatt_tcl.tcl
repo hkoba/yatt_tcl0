d060fd754fb15e9cabc4e030127fa67dae98b1a1
Kobayasi, Hiroaki <buribullet@gmail.com>
2019-01-01 17:32:24 +0900

    WIP: transpile

--- yatt_tcl.tcl
+++ yatt_tcl.tcl
@@ -7,8 +7,9 @@ package require textutil
 snit::type yatt_tcl {
     option -doc-root
     option -namespace-list [list yatt tcl]
+    option -template-ext .html
     
-    variable myMtime -array []
+    variable myCompileCache -array []
     
     method render {widgetName argDict} {
         set widgetPath [split $widgetName :]
@@ -19,6 +20,41 @@ snit::type yatt_tcl {
 
     }
     
+    method generate-script {widgetName} {
+        set fn $options(-doc-root)/$widgetName$options(-template-ext)
+        set declDict [$self parse-decllist [$self read_file $fn]]
+        $self transpile $declDict
+    }
+
+    method transpile {declDict {otherDecls ""}} {
+        set script []
+        foreach partSpec [dict keys $declDict] {
+            lassign $partSpec kind partName
+            append script [$self transpile-$kind $partName \
+                               [dict get $declDict $partSpec] $otherDecls]
+        }
+        set script
+    }
+
+    method transpile-page {partName declRec otherDecls} {
+        $self transpile-widget $partName $declRec $otherDecls
+    }
+    method transpile-widget {partName declRec otherDecls} {
+        set scriptBody []
+        foreach {text tok} [$self parse-body [dict get $declRec source]] {
+            if {$text ne ""} {
+                lappend scriptBody [list CON write $text]
+            }
+            if {[regexp {^&\w+:(\w+);} $tok -> varName]} {
+                lappend scriptBody [string map [list @VAR@ $varName] {CON write [escape $@VAR@]}]
+            } else {
+                lappend scriptBody "(($tok))"
+            }
+        }
+        return ";proc render__$partName {[list CON {*}[dict get $declRec atts]]} {[join $scriptBody \;]}"
+    }
+    method transpile-action {partName declRec otherDecls} {}
+
     method {re body} {} {
         set reNS [join $options(-namespace-list) |]
         string map [list @NS@ (?:$reNS)] {(?x)
